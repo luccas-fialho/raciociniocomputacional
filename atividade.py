7de3d4a39ff02d34a8f7f9a0b459c4205314c5ba
Luccas Fialho <luccas.flh@gmail.com>
2024-04-01 21:46:24 -0300

    Atividade Somativa 1: versão 2

--- atividade.py
+++ atividade.py
@@ -17,66 +17,76 @@ while True:
   print("(9) Sair.")
   print("\n")
 
-  opcao_gerenciamento = int(input("Informe a opção desejada: "))
-  print("\n")
-
-  if opcao_gerenciamento == 1:
-    while True:
-      print("***** [ESTUDANTES] MENU DE OPERAÇÕES *****")
-      print("\n")
-      print("(1) Incluir.")
-      print("(2) Listar.")
-      print("(3) Atualizar.")
-      print("(4) Excluir.")
-      print("(9) Voltar ao menu principal.")
-      print("\n")
-
-      opcao_crud = int(input("Informe a opção desejada: "))
-
-      if opcao_crud == 9:
+  try:
+    opcao_gerenciamento = int(input("Informe a opção desejada: "))
+    print("\n")
+    if opcao_gerenciamento == 1:
+      while True:
+        print("***** [ESTUDANTES] MENU DE OPERAÇÕES *****")
         print("\n")
-        print("Voltando ao menu principal...")
-        print("\n")
-        break
-      elif opcao_crud == 1:
-        print("\n")
-        print("===== INCLUSÃO =====")
+        print("(1) Incluir.")
+        print("(2) Listar.")
+        print("(3) Atualizar.")
+        print("(4) Excluir.")
+        print("(9) Voltar ao menu principal.")
         print("\n")
 
-        nome_estudante = input("Informe o nome do estudante: ")
-        lista_estudantes.append(nome_estudante)
-        input("Pressione ENTER para continuar.")
-        print("\n")
-      elif opcao_crud == 2:
-        print("\n")
-        print("===== LISTAGEM =====")
-        print("\n")
+        try:
+          opcao_crud = int(input("Informe a opção desejada: "))
 
-        if len(lista_estudantes) == 0:
-          print("Não há estudantes cadastrados.")
+          if opcao_crud == 9:
+            print("\n")
+            print("Voltando ao menu principal...")
+            print("\n")
+            break
+          elif opcao_crud == 1:
+            print("\n")
+            print("===== INCLUSÃO =====")
+            print("\n")
+
+            nome_estudante = input("Informe o nome do estudante: ")
+            lista_estudantes.append(nome_estudante)
+            input("Pressione ENTER para continuar.")
+            print("\n")
+          elif opcao_crud == 2:
+            print("\n")
+            print("===== LISTAGEM =====")
+            print("\n")
+
+            if len(lista_estudantes) == 0:
+              print("Não há estudantes cadastrados.")
+              print("\n")
+            else:
+              for nome in lista_estudantes:
+                print(f"- {nome}")
+
+            input("Pressione ENTER para continuar.")
+            print("\n")
+          elif opcao_crud == 3 or opcao_crud == 4:
+            print("\n")
+            print("EM DESENVOLVIMENTO")
+            print("\n")
+          else:
+            print("\n")
+            print("Opção inválida! Digite novamente...")
+            print("\n")
+        except ValueError:
           print("\n")
-        else:
-          for nome in lista_estudantes:
-            print(f"- {nome}")
+          print("Deve ser um número, não letra!")
+          print("\n")
 
-        input("Pressione ENTER para continuar.")
-        print("\n")
-      elif opcao_crud == 3 or opcao_crud == 4:
-        print("\n")
-        print("EM DESENVOLVIMENTO")
-        print("\n")
-      else:
-        print("\n")
-        print("Opção inválida! Digite novamente...")
-        print("\n")
-  elif opcao_gerenciamento == 2 or opcao_gerenciamento == 3 or opcao_gerenciamento == 4 or opcao_gerenciamento == 5:
-    print("EM DESENVOLVIMENTO")
+    elif opcao_gerenciamento == 2 or opcao_gerenciamento == 3 or opcao_gerenciamento == 4 or opcao_gerenciamento == 5:
+      print("EM DESENVOLVIMENTO")
+      print("\n")
+    elif opcao_gerenciamento == 9:
+      print("===== ATUALIZAÇÃO =====")
+      print("\n")
+      print("Finalizando aplicação...")
+      break
+    else:
+      print("Opção inválida! Digite novamente...")
+      print("\n")
+  except ValueError:
     print("\n")
-  elif opcao_gerenciamento == 9:
-    print("===== ATUALIZAÇÃO =====")
-    print("\n")
-    print("Finalizando aplicação...")
-    break
-  else:
-    print("Opção inválida! Digite novamente...")
+    print("Deve ser um número, não letra!")
     print("\n")
