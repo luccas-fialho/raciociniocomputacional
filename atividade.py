204e0762902fe47e814e81906bac946d4ac1f993
Luccas Fialho <luccas.flh@gmail.com>
2024-04-11 17:12:39 -0300

    AF5: listagem correta, edição e exclusão

--- atividade.py
+++ atividade.py
@@ -7,15 +7,13 @@ lista_estudantes = []
 
 # loop principal do sistema
 while True:
-  print("----- MENU PRINCIPAL -----")
-  print("\n")
+  print("----- MENU PRINCIPAL -----\n")
   print("(1) Gerenciar estudantes.")
   print("(2) Gerenciar professores.")
   print("(3) Gerenciar disciplinas.")
   print("(4) Gerenciar turmas.")
   print("(5) Gerenciar matrículas.")
-  print("(9) Sair.")
-  print("\n")
+  print("(9) Sair.\n")
 
   # Try/Except para lidar com entrada de caracteres inválidos
   try:
@@ -24,57 +22,61 @@ while True:
 
     if opcao_gerenciamento == 1:
       while True:
-        print("***** [ESTUDANTES] MENU DE OPERAÇÕES *****")
-        print("\n")
+        print("***** [ESTUDANTES] MENU DE OPERAÇÕES *****\n")
         print("(1) Incluir.")
         print("(2) Listar.")
         print("(3) Atualizar.")
         print("(4) Excluir.")
-        print("(9) Voltar ao menu principal.")
-        print("\n")
+        print("(9) Voltar ao menu principal.\n")
 
         # Outro Try/Except para lidar com caracteres inválidos
         try:
           opcao_crud = int(input("Informe a opção desejada: "))
 
           if opcao_crud == 9:
-            print("\n")
-            print("Voltando ao menu principal...")
-            print("\n")
+            print("\nVoltando ao menu principal...\n")
             break
           elif opcao_crud == 1:
-            print("\n")
-            print("===== INCLUSÃO =====")
-            print("\n")
-            cod_estudante = 0
-            nome_estudante = input("Informe o nome do estudante: ")
-            cpf_estudante = input("Informe o CPF do estudante: ")
+            print("\n===== INCLUSÃO =====\n")
+            jaCadastrado = False
+            cod_estudante = int(input("Informe o código do estudante: "))
 
-            dic = {}
+            for estudante in lista_estudantes:
+              if cod_estudante in estudante.values():
+                jaCadastrado = True
+                break
+            
+            if not jaCadastrado:
+              nome_estudante = input("Informe o nome do estudante: ")
+              cpf_estudante = input("Informe o CPF do estudante: ")
 
-            dic["codigo"] = cod_estudante + (len(lista_estudantes) + 1)
-            dic["nome"] = nome_estudante
-            dic["cpf"] = cpf_estudante
+              estudante = {}
 
-            lista_estudantes.append(dic)
+              estudante["codigo"] = cod_estudante 
+              estudante["nome"] = nome_estudante
+              estudante["cpf"] = cpf_estudante
+
+              lista_estudantes.append(estudante)
+            else:
+              print("\nCódigo de estudante já cadastrado!\n")
             input("Pressione ENTER para continuar.")
             print("\n")
           elif opcao_crud == 2:
-            print("\n")
-            print("===== LISTAGEM =====")
-            print("\n")
+            print("\n===== LISTAGEM =====\n")
             if len(lista_estudantes) == 0:
-              print("Não há estudantes cadastrados.")
-              print("\n")
+              print("Não há estudantes cadastrados.\n")
             else:
-              for nome in lista_estudantes:
-                print(f"- {nome}")
-            input("Pressione ENTER para continuar.")
+              for estudante in lista_estudantes:
+                cod = estudante["codigo"]
+                nome = estudante["nome"]
+                cpf = estudante["cpf"]
+                print(f"Código: {cod}, Nome: {nome}, CPF: {cpf}")
+            input("\nPressione ENTER para continuar.")
             print("\n")
           elif opcao_crud == 3:
+            print("\n===== ALTERAÇÃO =====\n")
             if len(lista_estudantes) == 0:
-              print("Não há estudantes cadastrados.")
-              print("\n")
+              print("Não há estudantes cadastrados.\n")
             else:
               cod_estudante = int(input("Digite o código do estudante a ser alterado: "))
               flag = True
@@ -89,15 +91,15 @@ while True:
                   estudante["codigo"] = cod_estudante
                   estudante["nome"] = nome_estudante
                   estudante["cpf"] = cpf_estudante
-                  print("Estudante alterado!\n")
+                  print("Informações do estudante alteradas!\n")
                   break
               if flag:
                 print("Estudante não encontrado!\n")
-              input("Pressione ENTER para continuar...")
+            input("Pressione ENTER para continuar...")
           elif opcao_crud == 4:
+            print("\n===== EXCLUSÃO =====\n")
             if len(lista_estudantes) == 0:
-              print("Não há estudantes cadastrados.")
-              print("\n")
+              print("Não há estudantes cadastrados.\n")
             else:
               cod_estudante = int(input("Digite o código do estudante a ser excluído: "))
               flag = True
@@ -110,27 +112,18 @@ while True:
                   break
               if flag:
                 print("Estudante não encontrado!\n")
-              input("Pressione ENTER para continuar...")
+            input("Pressione ENTER para continuar...")
           else:
-            print("\n")
-            print("Opção inválida! Digite novamente...")
-            print("\n")
+            print("\nOpção inválida! Digite novamente...\n")
         except ValueError:
-          print("\n")
-          print("A opção deve ser um número!")
-          print("\n")
+          print("\nA opção deve ser um número!\n")
     elif opcao_gerenciamento == 2 or opcao_gerenciamento == 3 or opcao_gerenciamento == 4 or opcao_gerenciamento == 5:
-      print("EM DESENVOLVIMENTO")
-      print("\n")
+      print("EM DESENVOLVIMENTO\n")
     elif opcao_gerenciamento == 9:
-      print("===== ATUALIZAÇÃO =====")
-      print("\n")
+      print("===== ATUALIZAÇÃO =====\n")
       print("Finalizando aplicação...")
       break
     else:
-      print("Opção inválida! Digite novamente...")
-      print("\n")
+      print("Opção inválida! Digite novamente...\n")
   except ValueError:
-    print("\n")
-    print("A opção deve ser um número!")
-    print("\n")
+    print("\nA opção deve ser um número!\n")
